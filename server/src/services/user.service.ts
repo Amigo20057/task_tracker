2c4dc62de9299abf04c340005eb4c2de1094c54d
Amigo20057 <denrizuk@gmail.com>
2025-10-01 18:13:51 +0300

    completed user service

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -16,7 +16,22 @@ export class UserService {
     });
   }
 
+  public async findUserById(id: string): Promise<User | null> {
+    return await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
   public async createUser(user: User): Promise<User> {
     return await this.prisma.user.create({ data: user });
+  }
+
+  public async profile(id: string): Promise<Omit<User, "password">> {
+    const foundedUser = await this.findUserById(id);
+    if (!foundedUser) throw new Error("User not found");
+    const { password, ...user } = foundedUser;
+    return user;
   }
 }
